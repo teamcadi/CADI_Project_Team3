88e81a0190015627c35a2aff962e5a0ddc1ef8d2
vividswan <vividswan@naver.com>
2020-12-29 15:31:43 +0900

    Update Team3Application.java

--- backend/src/main/java/com/cadi/team3/Team3Application.java
+++ backend/src/main/java/com/cadi/team3/Team3Application.java
@@ -2,7 +2,9 @@ package com.cadi.team3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Team3Application {
 

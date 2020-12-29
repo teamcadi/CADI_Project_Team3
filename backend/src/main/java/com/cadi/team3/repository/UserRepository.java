552876fc3982dded8ae866c26da201460274bfdb
vividswan <vividswan@naver.com>
2020-12-29 15:31:39 +0900

    Update UserRepository.java

--- backend/src/main/java/com/cadi/team3/repository/UserRepository.java
+++ backend/src/main/java/com/cadi/team3/repository/UserRepository.java
@@ -3,8 +3,7 @@ package com.cadi.team3.repository;
 import com.cadi.team3.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    public User findByUsername(String username);
 }

6d02b5065ac90bc4271b6c42d505b2bf715fe472
vividswan <vividswan@naver.com>
2021-01-19 22:39:46 +0900

    fix: 아이디에 영어소문자도 입력할 수 있도록 Valid 수정

--- backend/src/main/java/com/cadi/team3/account/SignupDto.java
+++ backend/src/main/java/com/cadi/team3/account/SignupDto.java
@@ -16,7 +16,7 @@ public class SignupDto {
 
     @NotBlank
     @Length(min=3, max = 20)
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9_-]{3,20}$")
     private String nickname;
 
     @NotBlank
